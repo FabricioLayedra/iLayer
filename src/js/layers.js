41f8c1195076ced8de7f23be1e86898fbd647874
Fabricio Layedra <flayedra@espol.edu.ec>
2019-03-07 15:57:18 -0800

    Physics by layer implemented. Opacity updated. Show/hide layers implemented. New layer added by template html. Layers elements in the side table linked to the svgs

--- src/js/layers.js
+++ src/js/layers.js
@@ -1,9 +1,291 @@
 var datafile = "https://raw.githubusercontent.com/FabricioLayedra/CiverseData/master/authors_relations_2016.json";
 
+var LAYERS = {};
+
+function create_svg(layer_name,width,height){
+    var draw = SVG(layer_name).size(width,height);
+    LAYERS[layer_name] = {"layer":draw,"physics-engine": ""};
+}
+
+function create_physics_worlds(layer_name){
+    Matter.use('matter-attractors');
+
+    // module aliases
+    var Engine = Matter.Engine,
+            World = Matter.World,
+            Bodies = Matter.Bodies;
+
+    // create an engine
+    var engine = Engine.create();
+
+
+    // create demo scene
+    var world = engine.world;
+
+    var ground = Bodies.rectangle(400, 610, 810, 60, {isStatic: true});
+    World.add(world, ground);
+    //Add more boundaries
+    Engine.run(engine);
+    LAYERS[layer_name]["physics-engine"] = engine;
+}
+
+//send a matter engine to stop it
+function stop_physics(engine){
+    engine.enabled = false;
+}
+    
+//send a matter engine to restart it
+function run_physics(engine){
+    engine.enabled = true;
+}   
+
+function add_gravity(engine){
+    if (engine.world.gravity.scale > 0){   
+        
+//        document.querySelector('#add-gravity > span.text.text-white-50').textContent = "Add gravity" ;
+        engine.world.gravity.scale = 0;
+    } else{
+//        document.querySelector('#add-gravity > span.text.text-white-50').textContent = "Remove gravity" ;
+        engine.world.gravity.scale = 0.000001;
+    }
+}
+
+$("#add-gravity").click(function (){
+
+    });
+
+var bodies = [];
+var Composite = Matter.Composite;
+
+(function render() {
+    for (let layer in LAYERS){
+//        console.log(LAYERS[layer])
+        if (LAYERS[layer]["physics-engine"].world){
+            bodies = Composite.allBodies(LAYERS[layer]["physics-engine"].world);
+            for (let i = 0; i < bodies.length; i += 1) {
+
+                let currentBody = bodies[i];
+                let fabricObject = currentBody.svg;
+
+
+                if (fabricObject) {
+                    let newX = currentBody.position.x;
+                    let newY = currentBody.position.y;
+                    fabricObject.attr("cx",newX);
+                    fabricObject.attr("cy",newY);
+
+        //            fabricObject.setPositionByOrigin({x: newX, y: newY}, 'center', 'center');
+        //            fabricObject.setCoords();
+                }
+            }
+        }
+    }
+    
+
+    window.requestAnimationFrame(render);
+    
+//
+//    
+//    
+//
+////    if (drawVertices) {
+////        context.fillStyle = '#fff';
+////        context.fillRect(0, 0, canvas.width, canvas.height);
+////        context.beginPath();
+////    }
+//
+
+//
+//    if (drawVertices) {
+//        context.lineWidth = 1;
+//        context.strokeStyle = '#999';
+//        context.stroke();
+//    }
+
+})();
+
+function showCoords(event) {
+  var x = event.clientX;
+  var y = event.clientY;    
+  return [x,y];
+}
+
+/* USING GRAPHLIB*/
+
+function addMovingEvents(fabricObject, nodeData) {
+    fabricObject.on({
+        moving: function (options) {
+            nodeData.inEdges.forEach(function (inEdge) {
+                var center = fabricObject.getPointByOrigin('center', 'center');
+                inEdge.path[1][3] = center.x;
+                inEdge.path[1][4] = center.y;
+            });
+            nodeData.outEdges.forEach(function (outEdge) {
+                var center = fabricObject.getPointByOrigin('center', 'center');
+                outEdge.path[0][1] = center.x;
+                outEdge.path[0][2] = center.y;
+            });
+        }
+    });
+}
+
+function addMouseEvents(object) {
+//    object.draggable();
+    
+    object.mousedown(function(){
+        object.mousedown = true;  
+    });
+    
+    object.touchmove(function(event){  
+        console.log("moving");
+        if (!object.mousedown)
+            return;
+        var pointer = showCoords(event);
+//        var pointer = canvas.getPointer(options.e);
+//        console.log(object.matter);
+
+//        Body.translate(object.matter, {
+//            x: (pointer[0] - object.matter.position.x) * 0.25,
+//            y: (pointer[1] - object.matter.position.y) * 0.25
+//        });
+        
+    });
+    
+    object.mouseup(function(){
+        object.mousedown = false;
+    });
+}
+
+function drawGraph(draw,g,layer) {
+
+    var nodeKeys = g.nodes();
+    var edges = g.edges();
+
+    // adding to the nodes objects of the graph both the fabric and the matter associated objects
+    nodeKeys.forEach(function (nodeKey) {
+
+        var nodeData = g.node(nodeKey);
+
+        // console.log("nodeData");
+        // console.log(nodeData);
+
+        nodeData.inEdges = new Array();
+        nodeData.outEdges = new Array();
+
+        var radius = 20;
+        var y = getRandomBetween(50, 800);
+        var x = getRandomBetween(50, 1000);
+
+//        var matterObject = Bodies.circle(x, y, radius);
+        
+        // NOTE THAT I AM NOT ADDING THE ELEMENTS OF THIS GRAPH TO THE PHYSICS WORLD IN THIS EXAMPLE
+//                    World.add(world, matterObject);
+
+        var fabricObject = draw.circle(radius).attr({
+        cx: x,
+        cy: y,
+        //Fill
+        fill: 'purple'
+        }).move(x,y);
+        addMouseEvents(fabricObject);
+
+//DO THIS WHEN ADD PHYSICS
+//        nodeData.matter = matterObject;
+        nodeData.svg = fabricObject;
+//
+//        matterObject.svg = fabricObject;
+//        fabricObject.matter = matterObject;
+//        matterObject.layer = "default";
+//
+//        fabricObject.layer = "default";
+
+        //canvas.add(fabricObject);
+
+//        addMouseEvents(fabricObject);
+
+        //addMovingEvents(fabricObject, nodeData);
+
+    });
+
+//    edges.forEach(function (edge) {
+//
+//        var from = g.node(edge.v);
+//        var to = g.node(edge.w);
+////        console.log("FROM-TO");
+////        console.log(from);
+////        console.log(to);
+//
+//        var fromCenterX = from.svg.cx();
+//        var fromCenterY = from.svg.cy();
+//
+//        var toCenterX = to.svg.cx();
+//        var toCenterY = to.svg.cy();
+//
+//        // creating a QUADRATIC CURVE. See https://www.sitepoint.com/html5-svg-quadratic-curves/ and http://fabricjs.com/quadratic-curve
+//        var path = "M" + fromCenterX + "," + fromCenterY + " Q" + (fromCenterX + (toCenterX - fromCenterX) / 2) + "," + (fromCenterY + (toCenterY - fromCenterY) / 2) + " " + toCenterX + "," + toCenterY;
+//
+//        var edgePath = draw.path(path).attr({
+//            stroke: 'white',
+//            fill: 'transparent',
+//            strokeWidth: 1
+////            lockMovementX: true,
+////            lockMovementY: true,
+////            hasControls: false,
+////            hasBorders: false,
+////            perPixelTargetFind: true,
+////            objectCaching: false
+//        });
+////        canvas.add(edgePath);
+////        edgePath.layer = "default";
+//
+//        to.inEdges.push(edgePath);
+//        from.outEdges.push(edgePath);
+//
+//    });
+    LAYERS[layer]["graph"] = g;
+}
+
+//Read the JSON file and draw the graph in a layer named after the parameter.
+function readDataColab(filename,layer_name){
+    $.getJSON(filename).done(function(json){
+        // Create a new directed graph
+        var g = new graphlib.Graph({directed: false});
+
+        var edges = json.links;
+        var nodes = json.nodes;
+
+        nodes.forEach(function (data) {
+            // console.log(data);
+            g.setNode(data["id"], {authorInfo: {name: data["id"], group:data["group"]}});
+        });
+
+        edges.forEach(function (data) {
+            g.setEdge(data["source"], data["target"], {colabInfo: {value: data["value"],id: data["id"]}});
+        });
+        var color = "#"+ add_new_layer(layer_name);
+        console.log(LAYERS[layer_name].layer);
+        drawGraph(LAYERS[layer_name].layer,g,layer_name);
+        var svg_id = $("#"+layer_name).children("svg").attr("id");
+        console.log(color);
+        console.log(SVG.get(svg_id).select("circle").fill(color));
+
+        
+
+
+    }).fail(function( jqxhr, textStatus, error ) {
+        var err = textStatus + ", " + error;
+        console.log( "Request Failed: " + err );
+    });
+    
+}
+//readDataColab(datafile);
+
+
 
 //----- List -----
 
 var active_color = null;
+
 
 var el = document.getElementById("layers-table");
 
@@ -13,15 +295,19 @@ var sortable = new Sortable(el,{
     //get the actual order
     // run a for loop to render things
     var list = document.getElementById("layers-table").getElementsByTagName("li");
-
-    for (var element of Array.prototype.slice.apply(list).reverse()){
-       var layer = element.querySelector('div > div:nth-child(1) > div.col-8.my-auto > input:nth-child(1)').getAttribute("value").toLowerCase().split(" ").join();
-      // render the layers
-      $(".set-canvases").prepend($(".canvas-"+layer).detach());
-    }
+    order_layers(list);
 }});
 
-
+function order_layers(list){
+    
+    for (var element of Array.prototype.slice.apply(list)){
+       var layer = element.querySelector('div > div:nth-child(1) > div.col-8.my-auto > input:nth-child(1)').getAttribute("value").toLowerCase().split(" ")[1];
+      // render the layers
+//      console.log(layer);
+      $("#set-canvases").prepend($("#"+layer).detach());
+    }
+    
+}
 function reset_color(color) {
 
   $(".color-active").css("stroke",color);
@@ -40,12 +326,23 @@ function random_id() {
 };
 
 
-function create_layer(){
-    //Add a new layer
-    var table = document.getElementById("layers-table");
+function create_layer(layer){
+    var canvas = document.createElement("div");
+    var container = document.getElementById("set-canvases");
+    console.log(container);
+    canvas.setAttribute("id",layer);
+    canvas.setAttribute("style","position: absolute");
+    container.appendChild(canvas);
+    create_svg(layer,1200,800);
+    
     var template = document.getElementById('li-element').content.cloneNode(true);
-    var id = random_id();
+    if (typeof layer === "undefined"){
+        var id = random_id();
+    }else{
+        var id = layer;
+    }
     change_layer_names(template,id);
+    add_events(template);
     return template;
 }
 
@@ -60,6 +357,19 @@ function change_layer_names(item,id){
     $(item.querySelector('div > div.row.collapse')).attr("id","collapse-"+id);
     $(item.querySelector('div > div.row.collapse > div > div.row > h6:nth-child(2)')).attr("id","opacity-"+id);
     $(item.querySelector('div > div.row.collapse > div > div.collapse-item.slidecontainer > input')).attr("id","range-"+id);
+    $(item.querySelector('div > div.row.collapse  > div:nth-child(2) > div.collapse-item.slidecontainer > div > div.col-4 > input')).attr("id","gravity-handler-"+id);
+}
+
+function add_events(item){
+    //checkbox opacity
+    $(item.querySelector('div > div:nth-child(1) > div.col-sm-auto.pr-0 > input')).change(function(){show_hide_layer(this);});
+    console.log("ADDING EVENTS");
+    //checkbox physics
+    $(item.querySelector('div > div.row.collapse  > div:nth-child(2) > div.collapse-item.slidecontainer > div > div.col-4 > input')).change(function(){gravity_handler(this);});
+    //button
+    $(item.querySelector('div > div:nth-child(1) > div.col-2 > button')).click(function(){stop_drag();});
+    //range
+    $(item.querySelector('div > div.row.collapse > div > div.collapse-item.slidecontainer > input')).on("input",function(){console.log("run");opacity_changer(this);});
 }
 
 function add_item(){
@@ -196,13 +506,33 @@ function add_world(layer_name){
     console.log(layer_name);
 }
 
-$("#new-layer").click(function(){
-    var layer = create_layer();
+function add_new_layer(layer_name){
+    var layer = create_layer(layer_name);
     var color = Math.floor(Math.random()*16777215).toString(16);
     $(layer.querySelector(' div > div:nth-child(1) > div.col-2-auto.mr-1')).attr("style","background-color:#"+color+"; height: auto; width: 5px"); 
+
     $("#layers-table").append(layer);
-    
+//            .select("circle").attr("fill",color));
+    return color;
+}
+
+$("#new-layer").click(function(){
+//    add_new_layer(random_id());
+    readDataColab(datafile,random_id());
 });
+
+function show_hide_layer(checkbox){
+    var layer = "#" + $(checkbox).attr("layer").toLowerCase().split(" ");
+    console.log(layer);
+    if (checkbox.checked){
+        $(layer).css("display","block");
+    }else{
+        $(layer).css("display","none");
+    }
+    // else{
+    //     // d3.selectAll(layer).style("opacity",0);
+    // }
+};
 
 $(":checkbox").change(function(){
     //console.log(this);
@@ -219,14 +549,133 @@ $(":checkbox").change(function(){
     // }
 });
 
+function opacity_changer(range){
+//   sortable["options"]["disabled"] = false;
+   console.log(sortable["options"]["disabled"]);
+    var layer_name = range.id.split("-")[1];
+    $("#opacity-"+layer_name).html(range.value);
+    //SEARCH FOR SELLECT ALL THE ELEMENTS OF THE LAYERS
+    SVG.get(get_svg_id(layer_name)).select("circle").attr("fill-opacity",range.value/100);
+    //
+    //d3.selectAll("."+layer_name).style("opacity",this.value/100);
+    //CHANGE TO NEW LIBRARY
+};
+//document.querySelector('#collapse-pfk6r > div > div.collapse-item.slidecontainer > input')
 
+function add_elements_to_world(world,layer){
+    var Bodies = Matter.Bodies;
+    var World = Matter.World;
+    
+    var g = LAYERS[layer]["graph"];
+    var nodeKeys = g.nodes();
+    var edges = g.edges();
+
+    // adding to the nodes objects of the graph both the fabric and the matter associated objects
+    nodeKeys.forEach(function (nodeKey) {
+
+        var nodeData = g.node(nodeKey);
+        var fabricObject = nodeData.svg;
+        var x = fabricObject.cx();     
+        var y = fabricObject.cy();
+        var radius = fabricObject.attr("r");
+        console.log(fabricObject);        
+        console.log(x,y,radius);
+
+
+        var matterObject = Bodies.circle(x, y, radius);
+        nodeData.matter = matterObject;
+        matterObject.svg = fabricObject;
+        fabricObject.matter = matterObject;
+        
+        World.add(world,matterObject);
+        
+//        matterObject.layer = "default";
+//
+//        fabricObject.layer = "default";
+
+        //canvas.add(fabricObject);
+
+//        addMouseEvents(fabricObject);
+
+        //addMovingEvents(fabricObject, nodeData);
+
+    });
+//
+//    edges.forEach(function (edge) {
+//
+//        var from = g.node(edge.v);
+//        var to = g.node(edge.w);
+////        console.log("FROM-TO");
+////        console.log(from);
+////        console.log(to);
+//
+//        var fromCenterX = from.svg.cx();
+//        var fromCenterY = from.svg.cy();
+//
+//        var toCenterX = to.svg.cx();
+//        var toCenterY = to.svg.cy();
+//
+//        // creating a QUADRATIC CURVE. See https://www.sitepoint.com/html5-svg-quadratic-curves/ and http://fabricjs.com/quadratic-curve
+//        var path = "M" + fromCenterX + "," + fromCenterY + " Q" + (fromCenterX + (toCenterX - fromCenterX) / 2) + "," + (fromCenterY + (toCenterY - fromCenterY) / 2) + " " + toCenterX + "," + toCenterY;
+//
+//        var edgePath = draw.path(path).attr({
+//            stroke: 'black',
+//            fill: 'transparent',
+//            strokeWidth: 1
+////            lockMovementX: true,
+////            lockMovementY: true,
+////            hasControls: false,
+////            hasBorders: false,
+////            perPixelTargetFind: true,
+////            objectCaching: false
+//        });
+////        canvas.add(edgePath);
+////        edgePath.layer = "default";
+//
+//        to.inEdges.push(edgePath);
+//        from.outEdges.push(edgePath);
+//
+//    });
+
+}
+function gravity_handler(checkbox){
+    var layer = checkbox.id.split("-")[2];
+
+    if (checkbox.checked){
+        
+        console.log(checkbox.id)
+        //Initialize the engine if it is not created
+        var engine; 
+        if (LAYERS[layer]["physics-engine"].length===0){
+            create_physics_worlds(layer);
+            engine = LAYERS[layer]["physics-engine"];
+            console.log(LAYERS[layer]["physics-engine"]);
+            //add elements to physics world
+            add_elements_to_world(engine.world,layer);
+        }else{
+            engine = LAYERS[layer]["physics-engine"];
+        }
+        //Initialize the engine if it is not created
+
+        console.log("Adding physics...");
+    }else{
+        if (LAYERS[layer]["physics-engine"].length===0){
+            console.log("No engine");
+        }else{
+            console.log("Stoping engine...");
+//            stop_physics(LAYERS[layer]["physics-engine"]);
+               LAYERS[layer]["physics-engine"] = "";
+
+        }
+        console.log("Removing physics...");
+    }   
+}
 
 $("[type='range']").on("input",function(){
-    var layer_name = this.id.split("_")[1];
-    $("#opacity_"+layer_name).html(this.value);
-    d3.selectAll("."+layer_name).style("opacity",this.value/100);
-
-
+    var layer_name = this.id.split("-")[1];
+    $("#opacity-"+layer_name).html(this.value);
+    //d3.selectAll("."+layer_name).style("opacity",this.value/100);
+    //CHANGE TO NEW LIBRARY
 });
 
 // var slider = document.getElementById("myRange");
@@ -252,6 +701,14 @@ $("[type='range']").on("input",function(){
 //     }
 // });
 
+function stop_drag(){
+    console.log(sortable["options"]["disabled"]);
+    if (sortable["options"]["disabled"] === true){
+        sortable["options"]["disabled"] = false;
+    }else{
+        sortable["options"]["disabled"] = true;
+    }
+}
 $(".stop-drag").click(function(){
     if (sortable["options"]["disabled"] === true){
         sortable["options"]["disabled"] = false;
@@ -260,6 +717,7 @@ $(".stop-drag").click(function(){
     }
 });
 
+//FIX THIS 
 $("p[id|='p']").click(function () {
   // body...
     console.log("clickeando");
@@ -294,6 +752,9 @@ $("p[id|='p']").click(function () {
     
 });
 
+
+/* FILLING DOWN THE NAME OF THE LAYER */
+
 $("input[id|='p']").click(function () {
   // body...
     console.log("clickeando");
@@ -317,14 +778,15 @@ $("input[id|='p']").dblclick(function () {
     $(this).attr("readonly",false);
     // console.log(this);
 
-
-
 });
 
+
+function get_svg_id(layer_name){
+    return $("#"+layer_name).children("svg").attr("id");
+}
 $("input[id|='p']").on('keypress',function(e) {
     $(this).attr("readonly",true);
     d3.select("."+$(this).attr("previous-layer-name")).attr("class",className);
-
 });
     // //$("."+value).css("box-shadow","10px 10px 5px red");
     // //$("."+value).css("stroke","10px 10px 5px red");
@@ -351,5 +813,6 @@ $("input[id|='p']").on('keypress',function(e) {
     // }
     
 
+readDataColab(datafile,"graph");
 
 
