ab62d93eeaa2247608efe117ae87dc13e58b61ad
Fabricio Layedra <flayedra@espol.edu.ec>
2019-03-07 17:25:21 -0800

    Search bar commented until we have zooming and panning.

--- src/js/search-bar.js
+++ src/js/search-bar.js
@@ -1,18 +1,18 @@
-$( function() {
-  $( "#search-bar" ).autocomplete({
-    //source: availableEntities,
-    select: function( event, ui ) {
-      console.log("Option selected");
-    },
-    close: function( event, ui ) {
-      console.log("Run selection")
-    }
-  });
-});
-
-
-$( function() {
-  $( "#search-bar").click(function(){
-      $("#search-bar").val("");
-    }
-  )}) ;
+//$( function() {
+//  $( "#search-bar" ).autocomplete({
+//    //source: availableEntities,
+//    select: function( event, ui ) {
+//      console.log("Option selected");
+//    },
+//    close: function( event, ui ) {
+//      console.log("Run selection")
+//    }
+//  });
+//});
+//
+//
+//$( function() {
+//  $( "#search-bar").click(function(){
+//      $("#search-bar").val("");
+//    }
+//  )}) ;
